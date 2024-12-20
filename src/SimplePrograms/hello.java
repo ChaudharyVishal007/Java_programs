29a1d0634ad3dbb12474546e0911c92e6a2d36f4
Vishal Chaudhary <vishalchaudhary132001@gmail.com>
2024-12-20 17:11:54 +0530

    new line_4

--- src/SimplePrograms/hello.java
+++ src/SimplePrograms/hello.java
@@ -7,7 +7,7 @@ public class hello {
 
     public static void main(String[] args) throws IOException {
         // The output message to be saved in the file
-        String message = "Hello world welcome to Jenkins..........run2";
+        String message = "Hello world welcome to Jenkins..........run2 ---> vishal";
 
         // Display the message on the console
         System.out.println(message);
