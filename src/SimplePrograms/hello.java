c64f96f839ec71c2f39ef048dc058ce43e73fbd6
Vishal Chaudhary <vishalchaudhary132001@gmail.com>
2024-12-20 13:17:26 +0530

    code update

--- src/SimplePrograms/hello.java
+++ src/SimplePrograms/hello.java
@@ -1,11 +1,21 @@
 package SimplePrograms;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class hello {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // The output message to be saved in the file
+        String message = "Hello world welcome to Jenkins..........";
 
-	    System.out.println("Hello world wlcome to jenkis..........");
-
-
-	}
-	}
+        // Display the message on the console
+        System.out.println(message);
+        
+        // Write the message to a file named "output.txt"
+        try (FileWriter fileWriter = new FileWriter("hello world.txt")) {
+            fileWriter.write(message);
+            System.out.println("Output has been successfully written to output.txt");
+        } 
+}
+    }
